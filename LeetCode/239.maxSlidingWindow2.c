134bed1ba599d9e45d23686a198589d4766797f3
qinglianyan <921049836@qq.com>
2023-11-20 11:25:57 +0800

    1120.239

--- LeetCode/239.maxSlidingWindow2.c
+++ LeetCode/239.maxSlidingWindow2.c
@@ -25,12 +25,14 @@ bool isEmpty(){
 }
 int popFront(){
     if(isEmpty()){
+        printf("error:popFront empty!\n");
         return -1;
     }
     return queue[qTop++];
 }
 int popBack(){
     if(isEmpty()){
+        printf("error:popBack empty!\n");
         return -1;
     }
     return queue[--qEnd];
@@ -45,7 +47,14 @@ void push(int value){
     while(!isEmpty()&&value>queue[qEnd-1]){
         popBack();
     }
-    queue[qEnd++]=value;
+    if(qEnd==qTop&&qEnd==-1){
+        qTop=0;
+        qEnd=1;
+        queue[qTop]=value;
+    }
+    else{
+        queue[qEnd++]=value;
+    }
     return ;
 }
 
@@ -57,6 +66,11 @@ int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
     int max;
     int *out=(int *)malloc(100000*(sizeof(int)));
     int outop=0;
+    qTop=-1;
+    qEnd=-1;
+    // returnSize=(int *)malloc(sizeof(int));
+    //下面这行不能删除，对leetcode里面用returnSize需要先进行初始化，否则不一定有值
+    *returnSize=0;
 
     for(i=0;i<k;i++){
         push(nums[i]);
@@ -71,20 +85,20 @@ return out;
 }
 
 int main(){
-    // int input[8]={1, 3, -1, -3, 5, 3, 6, 7};
-    // int k=3;
-    // int numsSize=8;
+    int input[8]={1, 3, -1, -3, 5, 3, 6, 7};
+    int k=3;
+    int numsSize=8;
 
-    int input[1]={1};
-    int k=1;
-    int numsSize=1;
+    // int input[1]={1};
+    // int k=1;
+    // int numsSize=1;
 
     int * output;
-    int returnSize=0;
+    int * returnSize=(int *)malloc(sizeof(int));
     int i, j;
 
-    output=maxSlidingWindow(input, numsSize, k, &returnSize);
-    for(i=0;i<returnSize-1;i++){
+    output=maxSlidingWindow(input, numsSize, k, returnSize);
+    for(i=0;i<(*returnSize)-1;i++){
         printf("%d,", output[i]);
     }printf("%d", output[i]);
     printf("\n");
