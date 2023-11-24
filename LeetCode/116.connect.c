7129f73d02aaa4e28a0c9d6c004ed499d1f0dcac
qinglianyan <921049836@qq.com>
2023-11-24 16:02:12 +0800

    not finished

---
+++ LeetCode/116.connect.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+struct Node {
+    int val;
+    struct Node *left;
+    struct Node *right;
+    struct Node *next;
+};
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct Node *left;
+ *     struct Node *right;
+ *     struct Node *next;
+ * };
+ */
+
+struct Node* connect(struct Node* root) {
+	
+}
+int main(){
+    int i, j;
+    struct Node *root,  *out,
+                *node1, *node2, *node3, *node4, *node5, *node6, *node7;
+    node1=(struct Node *)malloc(sizeof(struct Node));
+    node2=(struct Node *)malloc(sizeof(struct Node));
+    node3=(struct Node *)malloc(sizeof(struct Node));
+    node4=(struct Node *)malloc(sizeof(struct Node));
+    node5=(struct Node *)malloc(sizeof(struct Node));
+    node6=(struct Node *)malloc(sizeof(struct Node));
+    node7=(struct Node *)malloc(sizeof(struct Node));
+    
+    
+    return 0;
+}
