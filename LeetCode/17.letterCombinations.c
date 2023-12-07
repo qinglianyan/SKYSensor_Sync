6ead1206eda3642dc2ad74809383679cb5b7f690
qinglianyan <921049836@qq.com>
2023-12-07 11:03:27 +0800

    1207

--- LeetCode/17.letterCombinations.c
+++ LeetCode/17.letterCombinations.c
@@ -5,23 +5,63 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int path[10];
+char path[10];
 int pathTop=0;
+char phone[10][10]={
+    "",     //0
+    "",     //1
+    "abc",  //2
+    "def",  //3
+    "ghi",  //4
+    "jkl",  //5
+    "mno",  //6
+    "pqrs", //7
+    "tuv",  //8
+    "wxyz"  //9
+};
+
+void backtracking(char *digits, char **out, int *returnSize, int index){
+    int i, j;
+    int num=0;
+    if(pathTop==strlen(digits)){
+        //找到一个，要存到out里
+        // path[pathTop++]='\0';
+        // printf("%s\n", path);
+        // pathTop--;
+        out[(*returnSize)]=(char *)malloc(sizeof(char)*5);
+        for(i=0;i<pathTop;i++){
+            out[(*returnSize)][i]=path[i];
+        }
+        out[(*returnSize)][i]='\0';
+        (*returnSize)++;
+        return;
+    }
+    num=digits[index]-'0';
+    for(i=0;i<strlen(phone[num]);i++){
+        path[pathTop]=phone[num][i];
+        pathTop++;
+        backtracking(digits, out, returnSize, index+1);
+        pathTop--;
+    }
+    return ;
+}
+
 char** letterCombinations(char* digits, int* returnSize) {
     char **out;
     *returnSize=0;
+    pathTop=0;
     if(strlen(digits)==0){
         return NULL;
     }
-    out=(char **)malloc(sizeof(char *)*(3*3*3*3+1));
-
+    out=(char **)malloc(sizeof(char *)*(4*4*4*4+1));
+    backtracking(digits, out, returnSize, 0);
 
     return out;
 }
 
 int main(){
     int i, j;
-    char digits[10]="23";
+    char digits[10]="5678";
     int *returnSize=(int *)malloc(sizeof(int));
     char **out;
 
@@ -29,5 +69,9 @@ int main(){
     for(i=0;i<(*returnSize);i++){
         printf("%s\n", out[i]);
     }
+
+    // for(i=0;i<10;i++){
+    //     printf("%d : %s\n", i, phone[i]);
+    // }
     return 0;
 }
---
+++ LeetCode/39.combinationSum.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+/**
+ * 不确定循环次数，用回溯
+ * 1、参数
+ * 2、停止条件。目前拿的数字和大于target，或者等于target
+ * 3、单次循环
+*/
+int path[100];
+int pathTop=0;
+int pathSum=0;
+
+int cmp(const void *p1, const void *p2){
+    return *(int *)p1-*(int *)p2;
+}
+
+void backtracking(int *candidates, int candidatesSize, int target, int index, int **out, int *returnSize, int **returnColumnSizes){
+    int i, j;
+    if(pathTop>150){
+        // 题目给的额外的出错条件
+        printf("error : pathTop TOO LARGE!\n");
+        return ;
+    }
+    if(pathSum>=target){// 停止条件
+        if(pathSum==target){
+            //找到了目标值，要记录在out里
+            // for(i=0;i<pathTop;i++){
+            //     printf("%d ", path[i]);
+            // }printf("\n");
+            out[(*returnSize)]=(int *)malloc(sizeof(int)*100);
+            (*returnColumnSizes)[(*returnSize)]=pathTop;
+            for(i=0;i<pathTop;i++){
+                out[(*returnSize)][i]=path[i];
+            }
+            (*returnSize)++;
+            return ;
+        }
+        else if(pathSum>target){
+            //超出了
+            return ;
+        }
+    }
+    //单步循环
+    for(i=index;i<candidatesSize;i++){
+        path[pathTop]=candidates[i];
+        pathSum+=candidates[i];
+        pathTop++;
+        backtracking(candidates, candidatesSize, target, i, out, returnSize, returnColumnSizes);
+        pathSum-=candidates[i];
+        pathTop--;
+    }
+    return ;
+}
+
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes) {
+    int i, j;
+    int **out;
+    *returnSize=0;
+    pathTop=0;
+    pathSum=0;
+    if(candidatesSize<=0){
+        return NULL;
+    }
+    out=(int **)malloc(sizeof(int *)*151);
+    (*returnColumnSizes)=(int *)malloc(sizeof(int )*151);
+
+    qsort(candidates, candidatesSize, sizeof(int), cmp);
+
+    backtracking(candidates, candidatesSize, target, 0, out, returnSize, returnColumnSizes);
+
+    return out;
+}
+
+int main(){
+    int i, j;
+    int **out;
+    int candidates[4]={2, 6, 3, 7};
+    int target=7;
+    int candidatesSize=4;
+
+    int *returnSize=(int *)malloc(sizeof(int));
+    int **returnColumnSizes=(int **)malloc(sizeof(int *));
+    
+    *returnSize=0;
+    out=combinationSum(candidates, candidatesSize, target, returnSize, returnColumnSizes);
+    for(i=0;i<(*returnSize);i++){
+        for(j=0;j<(*returnColumnSizes)[i];j++){
+            printf("%d ", out[i][j]);
+        }printf("\n");
+    }
+    return 0;
+}
