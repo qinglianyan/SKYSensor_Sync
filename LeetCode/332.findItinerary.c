08f93ca356567ee31c4957f8352315eae3b0b051
qinglianyan <921049836@qq.com>
2023-12-11 16:54:34 +0800

    1211

---
+++ LeetCode/332.findItinerary.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define HASH_MAX 17578  //26*26*26=17576
+#define HASH_PLINE 26
+int path[300][5];
+int pathTop=0;
+int hash[HASH_PLINE][HASH_PLINE];
+int StrToNum(char *s){
+    int slen=strlen(s);
+    if(slen!=3){
+        printf("Str LEN != 3, Error!\n");
+        return -1;
+    }
+    return (s[0]-'A'+1)*HASH_PLINE*HASH_PLINE+(s[1]-'A'+1)*HASH_PLINE+(s[2]-'A'+1);
+}
+char *NumToStr(int in){
+    char *ret=(char *)malloc(sizeof(char )*4);
+    ret[3]='\0';
+    ret[2]=(in%HASH_PLINE)-1+'A';
+    in/=HASH_PLINE;
+    ret[1]=(in%HASH_PLINE)-1+'A';
+    in/=HASH_PLINE;
+    ret[0]=(in%HASH_PLINE)-1+'A';
+    return ret;
+}
+
+char** findItinerary(char*** tickets, int ticketsSize, 
+                    int* ticketsColSize, int* returnSize) {
+    int i, j;
+    char **out;
+    int numTmp1, numTmp2;
+    (*returnSize)=0;
+    pathTop=0;
+    if(ticketsSize<=0){
+        printf("ticketsSize Error!\n");
+        return NULL;
+    }
+    for(i=0;i<ticketsSize;i++){
+        numTmp1=StrToNum(tickets[i][0]);
+        numTmp2=StrToNum(tickets[i][1]);
+        hash[numTmp1)][numTmp2]=1;
+
+    }
+
+    return out;
+}
+
+int main(){
+    int i, j, k;
+    char tickets[5][2][5]={{"JFK", "SFO"}, {"JFK", "ATL"}, 
+            {"SFO", "ATL"}, {"ATL", "JFK"}, {"ATL", "SFO"}};
+    int ticketsSize=5;
+    int ticketColSize[5]={2, 2, 2, 2, 2};
+    int *returnSize=(int *)malloc(sizeof(int ));
+    char **out;
+    // For Test
+        int num=0;
+    // End
+    *returnSize=0;
+
+    // printf("%s , %s ", tickets[0][0], tickets[1][0]);
+    
+    // Test
+        num=StrToNum(tickets[0][0]);
+        printf("num = %d\n", num);
+        printf("back = %s\n", NumToStr(num));
+    // End 
+
+    out=findItinerary(tickets, ticketsSize, ticketColSize, returnSize);
+    for(i=0;i<(*returnSize);i++){
+        printf("%s ", out[i]);
+    }
+    return 0;
+}
---
+++ LeetCode/47.permuteUnique.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+#define MAX_OUT 800
+int path[10];
+int pathTop=0;
+int hash[22];
+int cmp(const void *p1, const void *p2){
+    return *(int *)p1-*(int *)p2;
+}
+void backstacking(int *nums, int numsSize, int **out,
+                int index, int *returnSize, 
+                int **returnColumnSizes){
+    int i, j;
+    /**
+     * -10<=nums[i]<=10
+    */
+    
+    if(pathTop==numsSize){
+        // 返回条件
+        // 找到了三个数字，一种排列
+        // for(i=0;i<pathTop;i++){
+        //     printf("%d ", path[i]);
+        // }printf("\n");
+        out[(*returnSize)]=(int *)malloc(sizeof(int)*pathTop);
+        (*returnColumnSizes)[(*returnSize)]=pathTop;
+        for(i=0;i<pathTop;i++){
+            out[(*returnSize)][i]=path[i];
+        }
+        (*returnSize)++;
+        return;
+    }
+    // 单步循环
+    for(i=0;i<numsSize;i++){
+        // if(hash[i]==1){
+        //     continue;
+        // }
+        if(i>0&&nums[i]==nums[i-1]&&hash[i-1]==1){
+            continue;
+        }
+        if(hash[i]==0){
+            hash[i]=1;
+            path[pathTop++]=nums[i];
+            backstacking(nums, numsSize, out, index+1, returnSize, returnColumnSizes);
+            hash[i]=0;
+            pathTop--;
+        }
+    }
+
+    return ;
+}
+
+int** permuteUnique(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    int **out;
+    int i, j;
+    (*returnSize)=0;
+    pathTop=0;
+    if(numsSize<=0){
+        printf("numsSize Error!\n");
+        return NULL;
+    }
+    qsort(nums, numsSize, sizeof(int), cmp);
+    out=(int **)malloc(sizeof(int *)*MAX_OUT);
+    (*returnColumnSizes)=(int *)malloc(sizeof(int )*MAX_OUT);
+    backstacking(nums, numsSize, out, 0, returnSize, returnColumnSizes);
+    return out;
+}
+
+int main(){
+    int i, j;
+    int nums[3]={1, 1, 2};
+    int numsSize=3;
+    int **out;
+    int *returnSize=(int *)malloc(sizeof(int));
+    int **returnColumnSizes=(int **)malloc(sizeof(int *));
+    (*returnSize)=0;
+    out=permuteUnique(nums, numsSize, returnSize, returnColumnSizes);
+    for(i=0;i<(*returnSize);i++){
+        for(j=0;j<(*returnColumnSizes)[i];j++){
+            printf("%d ", out[i][j]);
+        }printf("\n");
+    }
+    return 0;
+}
