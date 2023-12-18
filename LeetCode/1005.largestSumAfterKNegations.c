f85c45b5b8f05da51f6df860ee788698d952c4c7
LaosuoDice <921049836@qq.com>
2023-12-18 22:53:06 +0800

    '1218'

--- LeetCode/1005.largestSumAfterKNegations.c
+++ LeetCode/1005.largestSumAfterKNegations.c
@@ -2,17 +2,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-int cmp(const int *p1, const int *p2){
-    return *(int *)p1-*(int *)p2;
+int cmp(const void *p1, const void *p2){
+    return abs(*(int *)p2)-abs(*(int *)p1);
 }
 int largestSumAfterKNegations(int* nums, int numsSize, int k) {
     int out=0;
     int i, j;
     qsort(nums, numsSize, sizeof(int), cmp);
     // for(i=0;i<numsSize;i++){
-    //     printf("nums[%d] = %d\n", i, nums[i]);
-    // }
-    
+    //     printf("%d ", nums[i]);
+    // }printf("\n");
+    for(i=0;i<numsSize;i++){
+        if(nums[i]<0&&k>0){
+            nums[i]*=-1;
+            k--;
+        }
+    }
+    if(k%2==1) nums[numsSize-1]*=-1;
+    for(i=0;i<numsSize;i++){
+        out+=nums[i];
+    }
     return out;
 }
 
@@ -20,10 +29,13 @@ int main(){
     int i ,j;
     int out=0;
 
-    int nums[3]={4, 2, 3};
-    int numsSize=3;
+    // int nums[3]={4, 2, 3};
+    // int numsSize=3;
+
+    int nums[5]={2, -3, -1, 5, -4};
+    int numsSize=5;
     
-    out=largestSumAfterKNegations(nums, numsSize, 1);
+    out=largestSumAfterKNegations(nums, numsSize, 2);
     printf("out = %d\n", out);
     return 0;
 }
