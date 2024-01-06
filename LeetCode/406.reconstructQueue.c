c9dab12ba4fcff5ed171c73b2552c42800a8aba4
qinglianyan <921049836@qq.com>
2024-01-06 18:38:01 +0800

    0106

--- LeetCode/406.reconstructQueue.c
+++ LeetCode/406.reconstructQueue.c
@@ -8,18 +8,23 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 #define PEOPLE_MAX 2001
-int cmp(const void **p1, const void **p2){
+int cmp(const void *p1, const void *p2){
     if((*(int **)p2)[0]==(*(int **)p1)[0]){
         return (*(int **)p1)[1]-(*(int **)p2)[1];
     }
     return (*(int **)p2)[0]-(*(int **)p1)[0];
 }
-void push(int **people, int peopleSize, 
-        int *peopleColSize, int **out,
-        int *in, 
-        int *returnSize, int **returnColumnSizes){
+void push(int *in, int **out, int *returnSize){
     int i, j;
-
+    if(in[1]>(*returnSize)){
+        printf("push Error!\n");
+        return ;
+    }
+    for(i=(*returnSize)+1;i>=in[i];i--){
+        out[i]=out[i-1];
+    }
+    out[in[1]]=in;
+    (*returnSize)++;
     return ;
 }
 
@@ -31,38 +36,22 @@ int** reconstructQueue(int** people,
     int **out;
     int mNumber=0;
     qsort(people, peopleSize, sizeof(people[0]), cmp);
-    // for(i=0;i<peopleSize;i++){
-    //     printf("[");
-    //     for(j=0;j<2;j++){
-    //         printf("%d ", people[i][j]);
-    //     }printf("]\n");
-    // }
+    for(i=0;i<peopleSize;i++){
+        printf("[");
+        for(j=0;j<2;j++){
+            printf("%d ", people[i][j]);
+        }printf("]\n");
+    }
     out=(int **)malloc(sizeof(int *)*peopleSize);
     (*returnColumnSizes)=(int *)malloc(sizeof(int )*peopleSize);
     *returnSize=0;
-
     for(i=0;i<peopleSize;i++){
-        push(people, peopleSize, peopleColSize, out, out[i], returnSize, returnColumnSizes);
-        
         out[i]=(int *)malloc(sizeof(int)*2);
         (*returnColumnSizes)[i]=2;
-        mNumber=0;
-        if(i==0){
-            out[i][0]=people[i][0];
-            out[i][1]=people[i][1];
-            (*returnSize)++;
-        }
-        else{
-            mNumber=0;
-            for(j=0;j<(*returnSize);j++){
-                if(out[j][0]>=people[i][0]){
-                    mNumber++;
-                }
-                if(mNumber>=people[i][1]){
-                    break;
-                }
-            }
-        }
+    }
+
+    for(i=0;i<peopleSize;i++){
+        push(people[i], out, returnSize);
     }
     return out;
 }
