bb61aa765e2e493e85416482d3ba95a4e1830710
laosuo <921049836@qq.com>
2024-02-05 16:51:27 +0800

    2024.02.05

--- LeetCode/494.findTargetSumWays.c
+++ LeetCode/494.findTargetSumWays.c
@@ -10,6 +10,17 @@
 int findTargetSumWays(int* nums, int numsSize, int target) {
     int out=0;
     int i, j;
+    int sum=0;
+    int left=0;
+    for(i=0;i<numsSize;i++){
+        sum+=nums[i];
+    }
+    // 存在left-right=target,left+right=sum
+    // 则 left=(sum+target)/2
+    left=(sum+target)/2;
+    // 到此为止，问题转化为了一个容量为left的背包问题
+    // 物品的质量和价值是其本身数值
+
 
     return out;
 }
