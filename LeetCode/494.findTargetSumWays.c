60a4f7a934c019c3ac5bf2012d213c544ad1c4bd
qinglianyan <921049836@qq.com>
2024-02-29 16:57:55 +0800

    0229

--- LeetCode/494.findTargetSumWays.c
+++ LeetCode/494.findTargetSumWays.c
@@ -12,6 +12,7 @@ int findTargetSumWays(int* nums, int numsSize, int target) {
     int i, j;
     int sum=0;
     int left=0;
+
     for(i=0;i<numsSize;i++){
         sum+=nums[i];
     }
@@ -19,18 +20,48 @@ int findTargetSumWays(int* nums, int numsSize, int target) {
     // 则 left=(sum+target)/2
     left=(sum+target)/2;
     // 到此为止，问题转化为了一个容量为left的背包问题
+    // 用一个大小为left的背包，从nums里面选物品，有多少种选法正好拿满
     // 物品的质量和价值是其本身数值
+    if((sum+target)%2==1) return 0;
+    if(abs(target)>sum) return 0;
+    //
+    int dp[numsSize][left+1];
+    memset(dp, 0, sizeof(dp));
 
+    dp[0][0]=1;
+    if(nums[0]<=left){
+        dp[0][nums[0]]=1;
+    }
+    // 下面这个自己没想出来
+    if(nums[0]==0){
+        dp[0][0]=2;
+    }
 
-    return out;
+    for(i=1;i<numsSize;i++){
+        for(j=left;j>=nums[i];j--){
+           dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i]]; 
+        }
+    }
+    for(i=0;i<numsSize;i++){
+        for(j=0;j<=left;j++){
+            printf("%d ", dp[i][j]);
+        }
+        printf("\n");
+    }
+    //
+    return dp[numsSize-1][left];
 }
 
 int main(){
     int i, j;
     int out;
-    int nums[5]={1, 1, 1, 1, 1};
-    int numsSize=5;
-    int target=3;
+    // int nums[5]={1, 1, 1, 1, 1};
+    // int numsSize=5;
+    // int target=3;
+
+    int nums[9]={0, 0, 0, 0, 0, 0, 0, 0, 1};
+    int numsSize=9;
+    int target=1;
 
     out=findTargetSumWays(nums, numsSize, target);
     printf("__out = %d__\n", out);
