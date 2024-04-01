2284a31d3ba11dcdec3404c5a0b342526c53265f
Laosuo <921049836@qq.com>
2024-04-02 00:56:53 +0800

    20240402

--- LeetCode/213.rob.c
+++ LeetCode/213.rob.c
@@ -4,48 +4,45 @@
 #include <string.h>
 #define MAX(a, b) a>b?a:b
 
+int robCount(int *nums, int numsSize, int bbegin, int eend){
+    int i, j;
+    int dp[numsSize];
+    memset(dp, 0, sizeof(dp));
+    dp[1]=nums[bbegin];
+    for(i=2;i<numsSize;i++){
+        dp[i]=MAX(dp[i-1], dp[i-2]+nums[i-1+bbegin]);
+    }
+    return dp[numsSize-1];
+}
+
 int rob(int* nums, int numsSize) {
     int i, j;
-    int dp[numsSize+1];
-    bool record_1[numsSize+1];// 记录第一家是否被偷了
-    /** dp[i]表示从第一家到第i家一共能偷多少钱
-     * 两种情况：
-     *  如果第i家不偷，d[i]=d[i-1]
-     *  如果第i家偷，那么i-1家就不能偷，d[i]=d[i-2]+nums[i];
-     * 初始化：d[0]=0, d[1]=nums[1]
+    /**
+     * 对于nums[numsSize],这个题目只有两种情况，
+     * 一种是去掉第一家，偷下标1到numsSize-1
+     * 另一种是去掉最后一家，投下标0到numsSize-2
     */
-    memset(dp, 0, sizeof(dp));
-    memset(record_1, 0, sizeof(record_1));
-    record_1[1]=true;
-    dp[1]=nums[0];
+    int ret1, ret2;
     if(numsSize==1){
-        return dp[1];
+        return nums[0];
     }
-    for(i=2;i<=numsSize;i++){
-        if(i==numsSize){
-            
-        }
-        else{
-            if(dp[i-1]>dp[i-2]+nums[i-1]){
-                dp[i]=dp[i-1];
-                record_1[i]=record_1[i-1];
-            }
-            else{
-                dp[i]=dp[i-2]+nums[i-1];
-                record_1[i]=record_1[i-2];
-            }
-        }
+    else if(numsSize==2){
+        return MAX(nums[0], nums[1]);
     }
-
-    return dp[numsSize];
+    else{
+        ret1=robCount(nums, numsSize, 0, numsSize-2);
+        ret2=robCount(nums, numsSize, 1, numsSize-1);
+        return MAX(ret1, ret2);
+    }
+    return 0;
 }
 
 int main(){
     int i, j;
-    int numsSize=4;
+    int numsSize=3;
     int * nums=(int *)malloc(sizeof(int)*numsSize);
     int out=-1;
-    nums[0]=1, nums[1]=2, nums[2]=3, nums[3]=1;
+    nums[0]=2, nums[1]=3, nums[2]=2;
     out=rob(nums, numsSize);
     printf("__out = %d__\n", out);
     return 0;
