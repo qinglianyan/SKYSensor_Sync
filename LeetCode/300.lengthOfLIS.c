d2ecef0e2c7c4ee1b1cc3f09f80d33d87a9cbf72
qinglianyan <921049836@qq.com>
2024-06-17 17:00:08 +0800

    20240617

--- LeetCode/300.lengthOfLIS.c
+++ LeetCode/300.lengthOfLIS.c
@@ -2,42 +2,52 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-// 理解错了，这版不对
-// 题目说的是子序列就可以，我理解成需要连续子序列了
+
 int lengthOfLIS(int* nums, int numsSize) {
     int i, j;
-    int numsStore[numsSize];
+    int dp[numsSize];
     int maxLength;
-    memset(numsStore, 0, sizeof(numsStore));
-    // numsStore[]表示此处连续递增数量
-
-    numsStore[0]=1;
-    maxLength=1;
+    int max=1;
+    memset(dp, 0, sizeof(dp));
+    /**
+     * dp[i]表示的是以nums[i]结尾的严格递增子序列
+     * 所以nums[i]对于每个j∈[0, i-1]都有
+     *  if nums[i]>nums[j] 
+     *      nums[i]=nums[j]+1;
+    */
+    dp[0]=1;
     for(i=1;i<numsSize;i++){
-        if(nums[i]>nums[i-1]){
-            numsStore[i]=numsStore[i-1]+1;
-            if(numsStore[i]>maxLength){
-                maxLength=numsStore[i];
+        dp[i]=1;
+        for(j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                if(dp[j]+1>dp[i]){
+                    dp[i]=dp[j]+1;
+                    if(dp[i]>max){
+                        max=dp[i];
+                    }
+                }
             }
         }
-        else{
-            numsStore[i]=1;
-        }
-        printf("nums=%d, numsStore=%d, max=%d\n", nums[i], numsStore[i], maxLength);
     }
-    return maxLength;
+    for(i=0;i<numsSize;i++){
+        printf("%d ", dp[i]);
+    }
+    return max;
 }
 
 int main(){
     int i, j;
-    int numsSize=8;
-    int nums[8]={10, 9, 2, 5, 3, 7, 101, 18};
+    // int numsSize=8;
+    // int nums[8]={10, 9, 2, 5, 3, 7, 101, 18};
 
     // int numsSize=6;
     // int nums[6]={0, 1, 0, 3, 2, 3};
 
-    // int numsSize=7;
-    // int nums[7]={7, 7, 7, 7, 7, 7, 7};
+    int numsSize=7;
+    int nums[7]={7, 7, 7, 7, 7, 7, 7};
+
+    // int numsSize=9;
+    // int nums[9]={1,3,6,7,9,4,10,5,6};
     int out=0;
     out=lengthOfLIS(nums, numsSize);
     printf("__out = %d__\n", out);
