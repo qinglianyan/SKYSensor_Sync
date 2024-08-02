5716372daf16b7f769c43d97f94e28cf74d22985
laosuo <921049836@qq.com>
2024-08-02 18:02:37 +0800

    0802

--- LeetCode/72.minDistance.c
+++ LeetCode/72.minDistance.c
@@ -10,48 +10,72 @@
  * 插入一个字符
  * 删除一个字符
  * 替换一个字符
- * 
+ *
  * 示例 1：
  * 输入：word1 = "horse", word2 = "ros"
  * 输出：3
  * 解释： horse -> rorse (将 'h' 替换为 'r') rorse -> rose (删除 'r') rose -> ros (删除 'e')
- * 
+ *
  * 示例 2：
  * 输入：word1 = "intention", word2 = "execution"
  * 输出：5
  * 解释： intention -> inention (删除 't') inention -> enention (将 'i' 替换为 'e') enention -> exention (将 'n' 替换为 'x') exention -> exection (将 'n' 替换为 'c') exection -> execution (插入 'u')
- * 
+ *
  * 提示：
  * 0 <= word1.length, word2.length <= 500
  * word1 和 word2 由小写英文字母组成
-*/
-#define MIN(a, b, c) 
-int minDistance(char* word1, char* word2) {
+ */
+#define MIN(a, b, c) (a > b ? (b > c ? c : b) : (a > c ? c : a))
+int minDistance(char *word1, char *word2)
+{
     int i, j;
-    int count=0;
-    int len1=strlen(word1), len2=strlen(word2);
-    int dp[len1+1][len2+1];
+    int count = 0;
+    int len1 = strlen(word1), len2 = strlen(word2);
+    int dp[len1 + 1][len2 + 1];
     /**
      * dp[i][j]表示从word1[0 - i-1]转化为word2[0 - j-1]
      * 需要的步数
      * if word1[i-1]==word2[j-1]
      *      dp[i][j]=dp[i-1][j-1]
      * else
-     *      
-    */
-
-    return 0;
+     *      dp[i][j]=min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
+     */
+    memset(dp, 0, sizeof(dp));
+    for(i=0;i<=len1;i++){
+        dp[i][0]=i;
+    }
+    for(j=0;j<=len2;j++){
+        dp[0][j]=j;
+    }
+    for(i=1;i<=len1;i++){
+        for(j=1;j<=len2;j++){
+            if(word1[i-1]==word2[j-1]){
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=MIN(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1;
+            }
+        }
+    }
+    // for(i=0;i<=len1;i++){
+    //     for(j=0;j<=len2;j++){
+    //         printf("%d ", dp[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    return dp[len1][len2];
 }
 
-int main(){
+int main()
+{
     int i, j;
-    int *word1, *word2;
-    int out=0;
-    word1=(char *)malloc(sizeof(char)*6);
-    word2=(char *)malloc(sizeof(char)*4);
+    char *word1, *word2;
+    int out = 0;
+    word1 = (char *)malloc(sizeof(char) * 6);
+    word2 = (char *)malloc(sizeof(char) * 4);
     strcpy(word1, "horse");
     strcpy(word2, "ros");
-    out=minDistance(word1, word2);
-    printf("__out = %d__\n");
+    out = minDistance(word1, word2);
+    printf("__out = %d__\n", out);
     return 0;
 }
