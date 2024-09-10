f827a11a47b1aa66f7f103788ea533fd0d0023ae
laosuo <921049836@qq.com>
2024-09-10 17:20:38 +0800

    20240910

--- LeetCode/67.addBinary.c
+++ LeetCode/67.addBinary.c
@@ -6,19 +6,26 @@ void swap(char *in){
     int i, j;
     char tmp;
     int len=strlen(in);
-    int left=0, right=len-2;// 注意跳过最后的'\0'
+    int left=0, right=len-1;// 注意跳过最后的'\0'
     while(left<right){
         tmp=in[right];
         in[right]=in[left];
         in[left]=tmp;
+        left++;
+        right--;
     }
-    
-    
+    return ;
 }
 char* addBinary(char* a, char* b) {
     int i, j;
     int len1=strlen(a), len2=strlen(b);
-
+    int jinwei=0;
+    swap(a);
+    swap(b);
+printf("a = %s\n", a);
+printf("b = %s\n", b);
+    
+    return a;
 }
 
 int main(){
